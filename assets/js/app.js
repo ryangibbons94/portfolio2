da424740039dae19f691d40989e84b0a330259a3
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-03-05 11:42:46 -0500

    made hemburger menu come from the top instead of side

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,16 @@
 const burger = document.querySelector("#icon");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+const logo = document.querySelector('#logo')
 
 
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
+
+logo.addEventListener("click", () => {
+    ul.classList.remove("show");
+    document.getElementById("icon").classList.remove("change");
+  });
 
 // Hamburger menu function
 burger.addEventListener("click", () => {
