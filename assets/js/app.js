fa23263dc0f613ec05db92fa621b96e72388d7ca
ryangibbons94 <96315276+ryangibbons94@users.noreply.github.com>
2022-05-30 15:42:49 -0400

    added more skill logos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,15 @@
 // making things into variables
 const burger = document.querySelector("#icon");
 const ul = document.querySelector("nav ul");
-const logo = document.querySelector('#logo')
-
+const logo = document.querySelector("#logo");
 
 // Select nav links
 const navLink = document.querySelectorAll(".nav-link");
 //removes nav list from screen when my logo is clicked
 logo.addEventListener("click", () => {
-    ul.classList.remove("show");
-    document.getElementById("icon").classList.remove("change");
-  });
+  ul.classList.remove("show");
+  document.getElementById("icon").classList.remove("change");
+});
 
 // Hamburger menu function
 burger.addEventListener("click", () => {
@@ -25,10 +24,10 @@ navLink.forEach((link) =>
   })
 );
 
+burger.addEventListener("click", onClickMenu);
 
-burger.addEventListener('click', onClickMenu)
-
-function onClickMenu(){
-		document.getElementById("icon").classList.toggle("change");
+function onClickMenu() {
+  document.getElementById("icon").classList.toggle("change");
 }
 
+//make h1 diff size
